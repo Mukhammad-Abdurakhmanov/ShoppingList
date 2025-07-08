171968632ab6baa72510cec5bc863af339b6fabc
Muxammad <muxammad0513@mail.ru>
2025-07-08 21:11:31 +0300

    Finish ShopItemViewModel

--- app/src/main/java/com/example/shoppinglist/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopItemActivity.kt
@@ -7,7 +7,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.shoppinglist.R
 
-class ShopItemActivity : AppCompatActivity() {
+class  ShopItemActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_item)
